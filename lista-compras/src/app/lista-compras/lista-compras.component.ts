6b8b89c73548a6407c04ea146ce12338d8c2022d
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-12-03 12:14:53 -0300

    Seção de Conceitos: Lista de Compras [classes, arrays, for, empty, ngFor e css condicional]

--- lista-compras/src/app/lista-compras/lista-compras.component.ts
+++ lista-compras/src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  item: string = "";
+  lista: ItemLista[] = [];
 
+  adicionarItem(){
+    let itemLista = new ItemLista();
+    itemLista.id = this.lista.length + 1;
+    itemLista.nome = this.item;
+
+    this.lista.push(itemLista);
+    this.item = "";
+    console.table(this.lista);
+  }
+
+  riscarItem(item: ItemLista){
+    item.isComprado = !item.isComprado;
+  }
+
+  limparLista(){
+    this.lista = [];
+  }
 }
