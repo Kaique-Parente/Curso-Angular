520a0f5beb4000e56dfa17eee33eb2e1f727533c
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-06 22:14:53 -0300

    Seção Arquitetura de Micro-Frontends com Module-Federation: Projeto de Gráfico finalizado utilizando Chart.js

--- microfront-ends/grafico/src/app/app.ts
+++ microfront-ends/grafico/src/app/app.ts
@@ -1,12 +1,45 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('grafico');
+export class App implements OnInit {
+  ngOnInit(): void {
+    const dados = [
+      { categoria: 'Eletronicos', valor: 10 },
+      { categoria: 'Roupas', valor: 15 },
+      { categoria: 'Decoração', valor: 5 },
+      { categoria: 'Ferramentas', valor: 20 },
+      { categoria: 'Acessórios', valor: 17 },
+    ];
+
+    const labels = dados.map((d) => d.categoria);
+    const valores = dados.map((d) => d.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Categorias Mais Vendidas',
+            data: valores,
+            backgroundColor: 'rgba(31, 82, 146, 0.6)'
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
